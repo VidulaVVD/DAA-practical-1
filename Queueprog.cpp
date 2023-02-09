b4fcb59051d3489de8ea97880489d3db4baf3a5e
VidulaVVD <97016289+VidulaVVD@users.noreply.github.com>
2023-02-09 14:43:57 +0530

    Add files via upload

---
+++ Queueprog.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+ 
+#define SIZE 1000
+ 
+class Queue
+{
+    int *arr;       
+    int capacity;   
+    int front;      
+    int rear;       
+    int count;      
+ 
+public:
+    Queue(int size = SIZE);     
+    ~Queue();                   
+ 
+    int dequeue();
+    void enqueue(int x);
+    int peek();
+    int size();
+    bool isEmpty();
+    bool isFull();
+};
+ 
+
+Queue::Queue(int size)
+{
+    arr = new int[size];
+    capacity = size;
+    front = 0;
+    rear = -1;
+    count = 0;
+}
+ 
+
+Queue::~Queue() {
+    delete[] arr;
+}
+ 
+
+int Queue::dequeue()
+{
+    
+    if (isEmpty())
+    {
+        cout << "Underflow\nProgram Terminated\n";
+        exit(EXIT_FAILURE);
+    }
+ 
+    int x = arr[front];
+    cout << "Removing " << x << endl;
+ 
+    front = (front + 1) % capacity;
+    count--;
+ 
+    return x;
+}
+ 
+
+void Queue::enqueue(int item)
+{
+    
+    if (isFull())
+    {
+        cout << "Overflow\nProgram Terminated\n";
+        exit(EXIT_FAILURE);
+    }
+ 
+    cout << "Inserting " << item << endl;
+ 
+    rear = (rear + 1) % capacity;
+    arr[rear] = item;
+    count++;
+}
+ 
+
+int Queue::peek()
+{
+    if (isEmpty())
+    {
+        cout << "Underflow\nProgram Terminated\n";
+        exit(EXIT_FAILURE);
+    }
+    return arr[front];
+}
+ 
+// Utility function to return the size of the queue
+int Queue::size() {
+    return count;
+}
+ 
+// Utility function to check if the queue is empty or not
+bool Queue::isEmpty() {
+    return (size() == 0);
+}
+ 
+// Utility function to check if the queue is full or not
+bool Queue::isFull() {
+    return (size() == capacity);
+}
+ 
+int main()
+{
+    // create a queue of capacity 5
+    Queue q(5);
+ 
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+ 
+    cout << "The front element is " << q.peek() << endl;
+    q.dequeue();
+ 
+    q.enqueue(4);
+ 
+    cout << "The queue size is " << q.size() << endl;
+ 
+    q.dequeue();
+    q.dequeue();
+    q.dequeue();
+ 
+    if (q.isEmpty()) {
+        cout << "The queue is empty\n";
+    }
+    else {
+        cout << "The queue is not empty\n";
+    }
+ 
+    return 0;
+}
